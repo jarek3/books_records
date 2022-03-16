ee8f38fe8fa91bddfb2c801601cdc5e084bd8bd6
Jarek3 <jarek.patrny@gmail.com>
2022-03-16 05:29:16 +0100

    Initial commit

---
+++ migrations/m220313_162504_user.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220313_162504_user
+ */
+class m220313_162504_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+  $this->execute (CREATE TABLE IF NOT EXISTS `user`
+                                           (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+  ENGINE = InnoDB;
+                  )
+        
+  $this->execute (INSERT INTO `user` (`id`, `email`, `password`, `authKey`) VALUES
+    (1, 'jarek.patrny@gmail.com', 'admin', '');)    
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220313_162504_user cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220313_162504_user cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ models/User.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $email
+ * @property string $password
+ * @property string|null $authKey
+ */
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['email'], 'required'],
+            [['email', 'password', 'authKey'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('modelUser', 'ID'),
+            'email' => Yii::t('modelUser', 'Email'),
+            'password' => Yii::t('modelUser', 'Password'),
+            'authKey' => Yii::t('modelUser', 'Auth Key'),
+        ];
+    }
+
+
+    // ----- IdentityInterface methods:
+
+    public static function findIdentity($id) {
+        return static::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null) {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getAuthKey() {
+        return $this->authKey;
+    }
+
+    public function validateAuthKey($authKey) {
+        return $this->authKey === $authKey;
+    }
+
+    // ----- Because of default LoginForm:
+
+    public static function findByUsername($email) {
+        return static::findOne(['email' => $email]);
+    }
+
+    public function validatePassword($password) {
+        return $this->password === sha1($password);
+    }
+
+    // ----- Because of view
+
+    public function getUsername($email) {
+        return $this->email;
+    }
+}
