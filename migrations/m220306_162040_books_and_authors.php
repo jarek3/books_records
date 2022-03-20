a393e4d08d809a396b8a43c1137972322d1611c0
Jarek3 <jarek.patrny@gmail.com>
2022-03-20 17:21:17 +0100

    Initial commit

--- migrations/m220306_162040_books_and_authors.php
+++ migrations/m220306_162040_books_and_authors.php
@@ -13,9 +13,9 @@ class m220306_162040_books_and_authors extends Migration
     public function safeUp()
     {
      $this->execute("CREATE TABLE IF NOT EXISTS `books` (
-     `id` int NOT NULL, AUTO_INCREMENT,
-     `title` varchar(255) NOT NULL,
-     `author` varchar(255) DEFAULT NULL,
+     `id` int NOT NULL AUTO_INCREMENT,
+     `title` VARCHAR(255) NOT NULL,
+     `author` VARCHAR(255) NOT NULL,
      `description` text DEFAULT NULL,
      PRIMARY KEY (`id`),
      INDEX `fk_books_has_authors_authors1_idx` (`authors_id` ASC),
@@ -33,21 +33,21 @@ class m220306_162040_books_and_authors extends Migration
      ENGINE=InnoDB;");
 
      $this->execute("CREATE TABLE IF NOT EXISTS `authors` (
-     `id` int NOT NULL,AUTO_INCREMENT,
-     `name` varchar(255) DEFAULT NULL,
-     `surname` varchar(255) DEFAULT NULL,
+     `id` int NOT NULL AUTO_INCREMENT,
+     `name` VARCHAR(255) DEFAULT NULL,
+     `surname` VARCHAR(255) DEFAULT NULL,
      PRIMARY KEY (`id`))
      ENGINE=InnoDB;");
                    
      $this->execute ("INSERT INTO `books` (`id`, `title`, `author`, `description`)
                      VALUES (1, 'Learn PHP 8', 'Steve Prettyman', 'In this book you will create a complete three-tier application using a natural process of building and testing modules within each tier.'),
                             (2, 'Jižní stezka Českem od východu k západu', 'Jan Hocek', 'Jižní stezka vede napříč celou zemí mezi nejzápadnějším a nejvýchodnějším bodem České republiky, prostupuje nádherné česko-slovenské pohraničí, vydává se přes jihomoravské vinohrady a svou pouť končí přechodem Šumavy a Českého lesa.'),
-                            (3, 'Českem od severu k jihu', 'Jan Hocek', '600 km dlouhá trasa od našeho nejsevernějšího bodu po bod nejjižnější.');
+                            (3, 'Českem od severu k jihu', 'Jan Hocek', '600 km dlouhá trasa od našeho nejsevernějšího bodu po bod nejjižnější.')
                     ");
      
      $this->execute ("INSERT INTO `authors` (`id`, `name`, `surname`)
                      VALUES (1, 'Steve', 'Prettyman'),
-                            (2, 'Jan', 'Hocek');
+                            (2, 'Jan', 'Hocek')
                     ");
 
     }
