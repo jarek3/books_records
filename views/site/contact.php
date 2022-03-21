e0e44f7d60f6233c919c9ed6598ac7590193539d
Jarek3 <jarek.patrny@gmail.com>
2022-03-21 01:33:29 +0100

    Initial commit

--- views/site/contact.php
+++ views/site/contact.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
         <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
+            Děkujeme, že jste nás kontaktovali. Odpovíme Vám co nejdříve.
         </div>
 
         <p>
