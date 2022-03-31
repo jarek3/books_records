2816a182d2e301718d6c6b19e8925bf25497fc8c
Jarek3 <jarek.patrny@gmail.com>
2022-03-31 05:59:58 +0200

    Initial commit

--- views/user/_form.php
+++ views/user/_form.php
@@ -12,11 +12,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'email')->input('email')?>
 
-    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true])?>
 
-    <?= $form->field($model, 'authKey')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'authKey')->textInput(['maxlength' => true])?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
