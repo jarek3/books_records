1cf1045027dd42d215e05170be2361d3d2504a30
Jarek3 <jarek.patrny@gmail.com>
2022-03-31 05:49:41 +0200

    Initial commit

--- migrations/m220306_162143_user.php
+++ migrations/m220306_162143_user.php
@@ -16,12 +16,13 @@ class m220313_162504_user extends Migration
      `id` INT NOT NULL AUTO_INCREMENT,
      `email` VARCHAR(255) NOT NULL,
      `password` VARCHAR(255) NOT NULL,
+     `authKey` VARCHAR(255) NULL,
      PRIMARY KEY (`id`),
      UNIQUE INDEX `email_UNIQUE` (`email` ASC))
      ENGINE = InnoDB;");
         
      $this->execute ("INSERT INTO `user` (`id`, `email`, `password`, `authKey`)
-                    VALUES (1, 'jarek.patrny@gmail.com', 'admin', '');"
+                    VALUES (1, `jarek.patrny@gmail.com`, `81fe8bfe87576c3ecb22426f8e57847382917acf`, ``);"
                     );
 
     }
