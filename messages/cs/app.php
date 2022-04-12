9bf16a934d769e6951b9ef3b8830d0680d5346e9
Jarek3 <jarek.patrny@gmail.com>
2022-04-12 02:32:41 +0200

    Initial commit

--- messages/cs/app.php
+++ messages/cs/app.php
@@ -1,4 +1,4 @@
 <?php
 return ['Books' =>'Knihy',
-    'Authors' => 'Autoři', 'Users' => 'Uživatelé', 'Create Books' => 'Přidat knihu', 'Create Authors' => 'Přidat autora', 'Create User' => 'Přidat uživatele', 'Save' => 'Uložit', 'Update' => 'Upravit', 'Email' => 'E-mail', 'Password' => 'Heslo', 'Auth Key' => 'Autentizační klíč', 'Name' => 'Jméno', 'Surname'=> 'Příjmení', 'Title' => 'Titul', 'Author' => 'Autor', 'Description' => 'Popis', 'Year_of_publication' =>'Rok vydání', 'Delete'=> 'Smazat'];
+    'Authors' => 'Autoři', 'Users' => 'Uživatelé', 'Create Books' => 'Přidat knihu', 'Create Authors' => 'Přidat autora', 'Create User' => 'Registrace uživatele', 'Save' => 'Uložit', 'Update' => 'Upravit', 'Email' => 'E-mail', 'Password' => 'Heslo', 'Auth Key' => 'Autentizační klíč', 'Name' => 'Jméno', 'Surname'=> 'Příjmení', 'Title' => 'Titul', 'Author' => 'Autor', 'Description' => 'Popis', 'Year_of_publication' =>'Rok vydání', 'Delete'=> 'Smazat'];
 ?>
