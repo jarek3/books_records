450850f11326b81c12dcdf73ced80ae7a71f8274
Jarek3 <jarek.patrny@gmail.com>
2022-04-21 03:32:36 +0200

    Initial commit

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -74,7 +74,7 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="float-left">&copy; Jarek <?= date('Y') ?></p>
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
